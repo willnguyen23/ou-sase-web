f27a13aabaddacfc322c02ac466d3dae0573a337
wvnguyen23 <wvnguyen23@gmail.com>
2025-07-08 22:09:49 -0500

    fixing images

--- src/ExecPage/MemberInfo.tsx
+++ src/ExecPage/MemberInfo.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import '/src/ExecPage/Exec.css'
+import daniel from './src/Images/daniel.png'
 
 interface MemberImages {
     id: string;
@@ -14,7 +15,7 @@ const memberImages: MemberImages[] = [
         id: "daniel",
         num: 1,
         alt: "daniel park - president",
-        src: "/src/Images/daniel.png",
+        src: "daniel",
         info: ["Daniel Park", "President", "Chemical Engineering", "Junior"]
     }, 
     {
